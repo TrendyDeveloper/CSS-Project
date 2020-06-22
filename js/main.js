4611f9ca95bb3b073a3f858b28714cb04ea1c895
TrendyDeveloper <sunair552@gmail.com>
2020-06-22 15:19:47 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+/* alert("Truthy & Falsey");
+ console.log(Boolean(undefined));
+ console.log(Boolean(null));
+ console.log(Boolean(NaN));
+ console.log(Boolean(""));
+ console.log(Boolean(0));
+ console.log(Boolean(false));
+ console.log(Boolean(true));
+ console.log("Hello World");
+ alert("Bonjour"); single
+ prompt("Bonjour");
+ let visitorName = prompt("What's your name : ");
+ console.log(visitorName);
+
+ let cbon = confirm("c'est bon?");
+ console.log(cbon);
+ console.log(typeof true);
+ let person1 = {
+   name: "foulan",
+   age: 9000,
+   isStudent: true,
+ };
+ console.log(7 % 5);
+  NaN
+ console.log(0 / 0);
+ console.log("Some random string" * 2);
+
+  Infinity
+ console.log(1 / 0);
+ console.log(Infinity * Infinity);
+ console.log(1e309);
+ let num = 0;
+ console.log(num);
+ num++;
+ console.log(num);
+ num--;
+ console.log(num);
+ let a = 5;
+ let b = 7;
+ a += b;
+ console.log(a);  prints 12
+ console.log( 6 >= Infinity )*/
+ //function sum(a,b){
+	 let sum=0;
+	 sum = a+b;
+	 return sum
+ }
+ console.log(sum(5,5));
